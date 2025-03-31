21d2c1a57034332c8deec5e91c6c20c437cb194b
Santhoshkailasam <Kailasam5107@gmail.com>
2025-03-31 22:08:11 +0530

    success

--- src/portfolio.js
+++ src/portfolio.js
@@ -8,6 +8,8 @@ import profile from "./asserts/profile.jpg";
 import Innothon from "./asserts/Kcg-innothon.jpg";
 import Webquest from "./asserts/kcg-webquest.jpg";
 import Hackathon from "./asserts/kcg-hackathon.jpg";
+import Spotify from "./asserts/spotifytemplate.png";
+import Parking from "./asserts/parking.png";
 function Portfolio() {
   return (
     <div>
@@ -49,11 +51,16 @@ function Portfolio() {
            <img src={profile} alt="Profile" className="profile-img" />
          </div>
      </div>
+     <div style={{display:"flex",flexDirection:"row",justifyContent:"space-between",width:"60%"}}>
+        <div>
+            <button  className="resume-btn">Download Resume</button>
+        </div>
      <div className="icons-container">
            <img src={github} alt="GitHub Icon" className="icon" />
            <img src={linkedin} alt="LinkedIn Icon" className="icon" />
            <img src={phone} alt="Phone Icon" className="icon" />
            <img src={email} alt="Email Icon" className="icon" />
+     </div>
      </div>
      <hr></hr>
      {/* Participation container */}
@@ -110,8 +117,53 @@ function Portfolio() {
 
           <div className="projectcard-container">
              <h3 className="title">Title</h3>
+             <p className="title-text">A visually appealing Spotify <br/>clone built using React 
+             <br/>Native, featuring a seamless user interface and smooth navigation. A perfect project 
+             to showcase front-end development skills using <br />React Native."</p>
+             <button className="project-btn">View Project</button>
           </div>
+          <div class="vertical-line"></div>
+          <div style={{display:"flex",flexDirection:"row"}}>
+          <div>
+          <img src={Spotify} alt="Spotify" className="Spotify" />
+          </div>
+          <hr className="hr-line"/>
+          <div className="projectcard-container2">
+             <h3 className="title2">Title</h3>
+             <p className="title-text">Parking App is a user-friendly React Native frontend 
+              that simplifies the parking experience.It features a <br />home screen displaying nearby
+               parking spots, a booking system for reserving spaces.</p>
+             <button className="project-btn">View Project</button>
+          </div>
+          </div>
+          <div class="vertical-line2"></div>
+          <img src={Parking} alt="Spotify" className="Parking" />
+          <hr className="hr-line3"></hr> 
 
+          {/* Certificate section */}
+
+          <div className="participation-container">
+          <div className="participation">
+            <h3 className="projects-text">Certificates</h3>
+          </div> 
+          <div>
+            <button  className="btn2">Explore More &gt;&gt;</button>
+          </div>
+        </div>
+        <div style={{display:"flex",flexDirection:"row"}}>
+        <div className="circleleft">
+                <h3 style={{color:"#313131",fontSize:25,margin:0,paddingLeft:7}}>&lt;</h3>
+            </div>
+        <div className="certificate-card">
+                   <h3 className="card-text">App <br />project</h3>
+                   <img src={Hackathon} alt="Project" className="innothon-img" />
+                   <p className="para">This is  the certificate for contribute on parking app project</p>
+                   <button className="btn3">View in detail</button>
+                 </div>
+                 <div className="circleright">
+                <h3 style={{color:"#313131",fontSize:25,margin:0,paddingLeft:7}}>&gt;</h3>
+            </div>
+            </div>
      </div>
   );
 }
